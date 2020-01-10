a129e2ef40d28266e7639a673cd5ccce8881bb14
javasetuts <javasetuts69>
2020-01-11 03:06:23 +0530

    Added html doc for datatypes & functions

--- datatypes-variables/datatypes.py
+++ datatypes-variables/datatypes.py
@@ -9,6 +9,35 @@
 x = 56
 y = 78.89
 
-type(x)
+# print(type(x))
+# print(type(y))
 
-type(y)
+x = 568
+
+def access_global_var():
+    print(x)
+
+def access_global_variable():
+    print(x)
+
+
+access_global_var()
+access_global_variable()
+
+class InstanceVar():
+    def __init__(self):
+        self.course = 'Python'
+        self.title = 'Python Instance Variable'
+        self.folks_count = 15
+
+obj = InstanceVar()
+print(obj.course)
+print(obj.title)
+print(obj.folks_count)
+
+
+def local_variable():
+    x = 67    # local variable
+    y = 89    # local variable
+    z = x + y
+    print(z)
